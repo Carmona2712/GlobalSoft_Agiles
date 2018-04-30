a739144c824ac6143073ea5c21380164c550c64a
Ricardo Carmona <Ricardo Carmona@DESKTOP-JN0CIEN>
2018-04-30 01:16:36 -0500

    Controlador y Clase Cliente

---
+++ AplicacionGlobalFoodTrading/src/aplicacionglobalfoodtrading/Controlador/ControladorCliente.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionglobalfoodtrading.Controlador;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Ricardo Carmona
+ */
+public class ControladorCliente {
+     private static Connection con;
+    private static ResultSet rs;
+    private static Statement st;
+
+    public static Connection getCon() {
+        return con;
+    }
+
+    public static void setCon(Connection aCon) {
+        con = aCon;
+    }
+
+    public static ResultSet getRs() {
+        return rs;
+    }
+
+    public static void setRs(ResultSet aRs) {
+        rs = aRs;
+    }
+
+    public static Statement getSt() {
+        return st;
+    }
+
+    public static void setSt(Statement aSt) {
+        st = aSt;
+    }
+
+    public ControladorCliente() {
+    }
+    
+    
+    
+    
+}
