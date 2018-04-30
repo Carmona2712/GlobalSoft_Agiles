6ee576c28106e3f985cb91b8bb174f31dca0a066
Ricardo Carmona <Ricardo Carmona@DESKTOP-JN0CIEN>
2018-04-30 01:38:25 -0500

    Agregado Conexion a la base de datos y registro de Clientes

--- AplicacionGlobalFoodTrading/src/aplicacionglobalfoodtrading/Controlador/ControladorCliente.java
+++ AplicacionGlobalFoodTrading/src/aplicacionglobalfoodtrading/Controlador/ControladorCliente.java
@@ -5,9 +5,14 @@
  */
 package aplicacionglobalfoodtrading.Controlador;
 
+import aplicacionglobalfoodtrading.Modelo.Categoria;
+import aplicacionglobalfoodtrading.Modelo.Cliente;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -46,6 +51,41 @@ public class ControladorCliente {
     }
     
     
+    public void Conectar() {
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_gft", "root", "");
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar BD");
+        }
+    }
+    
+    
+    public int RegistrarCliente(Cliente c) {
+        int exi = 0;
+        String sql = "INSERT INTO `cliente`(`tipoid`, `identificacion`, `nombres`, `apellidos`, `pais`, `ciudad`, `provincia`, `cod_postal`, `direccion`) VALUES ('"+c.getTipoid()+"','"+c.getIdentificacion()+"','"+c.getNombre()+"','"+c.getApellido()+"','"+c.getPais()+"','"+c.getCiudad()+"','"+c.getProvincia()+"','"+c.getCodPostal()+"')";
+        try {
+            Conectar();
+            //System.out.println(sql);
+            System.out.println(sql);
+            st = con.createStatement();
+            
+            if(st.executeUpdate(sql)>0){
+                exi = 1;
+            }else{
+                exi = 0;
+            }
+ 
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage() + " en el Controlador Cliente", "Error", JOptionPane.ERROR_MESSAGE);
+            // System.out.println(sql);
+            exi = 0;
+        }
+        
+        return exi;
+        
+    }
     
     
 }
