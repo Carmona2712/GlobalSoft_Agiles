09db4eba5a40c468254c9f80d0cf26a5663752bf
Brayan Ortega <>
2018-05-14 23:53:41 -0500

    Aqui se creo el controlador de Pedidos con su conexion a la base de datos..... faltan las funciones de la crud

---
+++ AplicacionGlobalFoodTrading/src/aplicacionglobalfoodtrading/Controlador/Controlador_Pedido.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionglobalfoodtrading.Controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ricardo Carmona
+ */
+public class Controlador_Pedido {
+
+    private static Connection con;
+    private static ResultSet rs;
+    private static Statement st;
+
+    public static Connection getCon() {
+        return con;
+    }
+
+    public static void setCon(Connection aCon) {
+        con = aCon;
+    }
+
+    public static ResultSet getRs() {
+        return rs;
+    }
+
+    public static void setRs(ResultSet aRs) {
+        rs = aRs;
+    }
+
+    public static Statement getSt() {
+        return st;
+    }
+
+    public static void setSt(Statement aSt) {
+        st = aSt;
+    }
+
+    public Controlador_Pedido() {
+    }
+    
+    
+    public void Conectar() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_gft", "root", "");
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar BD");
+        }
+    }
+    
+    
+
+}
