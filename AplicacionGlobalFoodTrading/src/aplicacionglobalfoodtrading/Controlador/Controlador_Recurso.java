b48e9b162cd847cbeb9dc1c0be6cddd9f8c5b902
Ricardo Carmona <>
2018-05-24 23:42:27 -0500

    Creada el controlador de recursos fisicos y su conexion a la base de datos---

---
+++ AplicacionGlobalFoodTrading/src/aplicacionglobalfoodtrading/Controlador/Controlador_Recurso.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionglobalfoodtrading.Controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class Controlador_Recurso {
+
+    private static Connection con;
+    private static ResultSet rs;
+    private static Statement st;
+
+    public Controlador_Recurso() {
+    }
+
+
+    public static Connection getCon() {
+        return con;
+    }
+    
+
+    public static void setCon(Connection aCon) {
+        con = aCon;
+    }
+
+    public static ResultSet getRs() {
+        return rs;
+    }
+
+    public static void setRs(ResultSet aRs) {
+        rs = aRs;
+    }
+
+    public static Statement getSt() {
+        return st;
+    }
+
+    public static void setSt(Statement aSt) {
+        st = aSt;
+    }
+    
+    public void Conectar() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_gft", "root", "");
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar BD");
+        }
+    }
+    
+}
